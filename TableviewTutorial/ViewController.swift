dd0b894eb01d9318d4add5b43622dd2d36ff361b
Adem MAMA <adem16fb@gmail.com>
2017-12-07 21:51:53 +0300

    application complete

--- TableviewTutorial/ViewController.swift
+++ TableviewTutorial/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var countryName = ["Türkiye","Azerbaycan","Rusya","ABD","Suriye"]
+    var countryImages = ["turkey.jpg","azerbaijan.jpg","russia.png","abd.png","syria.png"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            countryName.remove(at: indexPath.row)
+            countryImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = countryName[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countryName.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let VC = storyboard.instantiateViewController(withIdentifier: "vc2") as! ViewController2
+        VC.countryName = countryName[indexPath.row]
+        VC.countryImage = countryImages[indexPath.row]
+        self.navigationController?.pushViewController(VC, animated: true)
+    }
 
 }
 
--- TableviewTutorial/ViewController2.swift
+++ TableviewTutorial/ViewController2.swift
@@ -9,27 +9,14 @@
 import UIKit
 
 class ViewController2: UIViewController {
-
+    var countryName = ""
+    var countryImage = ""
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: countryImage)
+        label.text = countryName
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
